45b00e228e5a794daab6bda8eee14add64e98bb5
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-03-07 18:15:15 +0100

    login y registrer

--- app/src/main/java/com/example/socketpsp/MainActivity.java
+++ app/src/main/java/com/example/socketpsp/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.socketpsp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+   private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Configura un OnClickListener para el botón de inicio de sesión
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Método llamado cuando se hace clic en el botón
+                navigateToMainActivity(); // Método para iniciar la actividad MainActivity
+            }
+        });
+    }
+
+
+    private void navigateToMainActivity() {
+        // Crea un Intent para iniciar la actividad MainActivity
+        Intent intent = new Intent(this, Registrer.class);
+        startActivity(intent); // Inicia la actividad
     }
 }
