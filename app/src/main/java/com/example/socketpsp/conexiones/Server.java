e12b4b3ab41f4afe33e92e82ccd70f1735e507d1
Jose Espejo <jespagu2610@g.educaand.es>
2024-03-09 11:33:45 +0100

    Server y Client

---
+++ app/src/main/java/com/example/socketpsp/conexiones/Server.java
@@ -0,0 +1,72 @@
+package com.example.socketpsp.conexiones;
+
+import com.example.socketpsp.model.Ardilla;
+import com.example.socketpsp.model.Poema;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+public class Server {
+    private static final int PORT = 8080;
+    private static final int MAX_CLIENTS = 100;
+
+    // Mapa que mantiene un seguimiento de las conexiones de los clientes (Ardillas) y sus respectivos ObjectOutputStream
+    static Map<Ardilla, ObjectOutputStream> clientes = new HashMap<>();
+
+    // Mapa que mantiene un seguimiento de los poemas recogidos por cada Ardilla
+    static Map<Ardilla, Poema> poemasRecogidos = new HashMap<>();
+
+    // ExecutorService gestiona un conjunto de hilos para manejar las conexiones de los clientes
+    static ExecutorService pool = Executors.newFixedThreadPool(MAX_CLIENTS);
+
+    public static void main(String[] args) {
+        System.out.println("Servidor de Ardillas Atómicas está en ejecución...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                // Acepta nuevas conexiones de clientes y las delega a un nuevo hilo de ManejadorCliente
+                pool.execute(new ManejadorCliente(serverSocket.accept()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para enviar un poema a todos los clientes conectados
+    static void broadcast(Poema poema) throws IOException {
+        synchronized (clientes) {
+            for (ObjectOutputStream cliente : clientes.values()) {
+                cliente.writeObject(poema);
+                cliente.flush();
+            }
+        }
+    }
+
+    // Clase interna que maneja la conexión con un cliente individual
+    static class ManejadorCliente implements Runnable {
+        private Socket socket;
+
+        ManejadorCliente(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Establece un ObjectOutputStream para enviar datos al cliente
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                // Crea una nueva Ardilla y la agrega al mapa de clientes
+                Ardilla ardilla = new Ardilla();
+                clientes.put(ardilla, outputStream);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/socketpsp/model/Poema.java
@@ -0,0 +1,66 @@
+package com.example.socketpsp.model;
+
+import java.io.Serializable;
+
+public class Poema implements Serializable {
+    private int id;
+    private String titulo;
+    private String contenido;
+    private int puntos;
+
+    public Poema(int id, String titulo, String contenido, int puntos) {
+        this.id = id;
+        this.titulo = titulo;
+        this.contenido = contenido;
+        this.puntos = puntos;
+    }
+
+    public Poema(){
+        this.id = -1;
+        this.titulo = "";
+        this.contenido = "";
+        this.puntos = -1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    @Override
+    public String toString() {
+        return "Poema{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", contenido='" + contenido + '\'' +
+                ", puntos='" + puntos + '\'' +
+                '}';
+    }
+}
